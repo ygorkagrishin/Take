2fbb033d25d4013a2fc6066f3c6a3ad7c7fcb87b
ygorkagrishin <webygorkagrishin@gmail.com>
2018-05-04 19:52:24 +0300

    Доработан hoverIos.js и landscape в hero

--- build/main.js
+++ build/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){var t=$("#js-form");t.submit(function(e){e.preventDefault();var a=$(this).serialize();$.ajax({type:"POST",url:"action.php",data:a,success:function(){t.trigger("reset"),alert("Ваше сообщение отпрвлено!")}})})});
-!function(){if(navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/iPad/i)){var a=$(".some-box__table");a.click(function(){a.hasClass("some-box__table_hover");a.hasClass("some-box__table_is_active")?a.removeClass("some-box__table_is_active"):(a.removeClass("some-box__table_hover"),a.addClass("some-box__table_is_active"))})}}();
+!function(){if(navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)||navigator.userAgent.match(/iPad/i)){var a=$(".some-box__table");a.hasClass("hover")&&a.removeClass("some-box__table_hover"),a.on("click",function(){a.hasClass("active")?a.removeClass("active"):a.addClass("active")})}}();
 $("input, textarea").placeholder();
--- src/static/scripts/hoverIos.js
+++ src/static/scripts/hoverIos.js
@@ -1,23 +1,19 @@
 (function () {
-
-    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || 
-    (navigator.userAgent.match(/iPad/i))) {
-        
-        var currentElement = $('.some-box__table');
-
-        currentElement.click(function () {
-
-            var hoverSelector = currentElement.hasClass('some-box__table_hover'),
-                active = currentElement.hasClass('some-box__table_is_active');
-
-            if (!active) {
-                currentElement.removeClass('some-box__table_hover');
-                currentElement.addClass('some-box__table_is_active');
-            }
-            else {
-                currentElement.removeClass('some-box__table_is_active');
-            }
-        });
-    }
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || 
+(navigator.userAgent.match(/iPad/i))) {
     
+    var box = $('.some-box__table');
+
+    if (box.hasClass('hover')) {
+        box.removeClass('some-box__table_hover');
+    }
+
+    function handler() {
+        box.hasClass('active') ? 
+        box.removeClass('active') : box.addClass('active');
+    }
+
+    box.on('click', handler);
+
+}
 })();
